3f65c3a55f8755ff5dcaad5fc205252c1bc6eb88
Endorze <alexyurihallgren@hotmail.com>
2025-03-07 17:16:11 +0100

    feat: Added a proper filestructure, routed to correct pages.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
 import { BrowserRouter, Routes, Route, } from "react-router-dom";
+import Home from "./pages/Home/home";
+import About from "./pages/About/about";
+import Contact from "./pages/Contact/contact";
+import News from "./pages/News/news";
+import Shop from "./pages/Shop/shop";
 import './App.css'
 
 function App() {
@@ -7,11 +12,11 @@ function App() {
     <>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={""}/>
-        <Route path="/about" element={""}/>
-        <Route path="/contact" element={""}/>
-        <Route path="/news" element={""}/>
-        <Route path="/shop" element={""}/>
+        <Route path="/" element={<Home />}/>
+        <Route path="/about" element={<About />}/>
+        <Route path="/contact" element={<Contact />}/>
+        <Route path="/news" element={<News />}/>
+        <Route path="/shop" element={<Shop />}/>
       </Routes>
     </BrowserRouter>
     </>
---
+++ src/pages/Home/home.jsx
@@ -0,0 +1,14 @@
+import styles from "./home.module.css"
+
+const Home = () => {
+    return (
+        <>
+        <div>
+            <h1>Home</h1>
+            <img src="/carlexdesign.png" />
+        </div>
+        </>
+    )
+}
+
+export default Home;
