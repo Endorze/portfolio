82e4c4474862e30efea1a2099248fe39f8965e8a
Endorze <alexyurihallgren@hotmail.com>
2025-03-12 17:43:20 +0100

    feat: Added footer to homepage

---
+++ src/components/Footer/footer.jsx
@@ -0,0 +1,41 @@
+import Container from "../Container/container";
+import styles from "./footer.module.css"
+
+const Footer = () => {
+    return (
+        <div className={styles.footer}>
+            <Container>
+                <h2 className={styles.contactTitle}>Contact</h2>
+                <div className={styles.horizontal}>
+                    <div className={styles.generalInfo}>
+                        <h4>General Info</h4>
+                        <p>CARLEX, DD-STUDIO
+                            <br/>4234 Fake Adress
+                            <br/>Uppsala, SE 75X XX
+                            <br/>(+46)-709595287 
+                            <br/><a href="mailto: alexanderjimhallgren@gmail.com">Alexanderjimhallgren@gmail.com</a>
+                        </p>
+                    </div>
+                    <div className={styles.services}>
+                        <h4>My Pages</h4>
+                        <a href="https://kantherm.se/">Kantherm</a>
+                        <a href="https://express-multipage-assignment2.vercel.app/">Summoners War</a>
+                        <a href="https://react-mock-webpage.vercel.app/">Bethesda</a>
+                        <a href="https://endorze.github.io/pokemon-game/">Pokemon Game</a>
+                    </div>
+                    <div className={styles.media}>
+                        <h4>Social Media</h4>
+                        <a href="https://www.linkedin.com/feed/">LinkedIn</a>
+                        <a href="https://www.linkedin.com/feed/">Instagram</a>
+                        <a href="https://www.linkedin.com/feed/">Facebook</a>
+                    </div>
+                </div>
+                <div className={styles.copyright}>
+                    <p>&copy; 2025 <a href="/">CARLEX DD STUDIO</a>. All rights reserved.<br/> This website, design and all rights belongs to Alexander Hallgren</p>
+                </div>
+            </Container>
+        </div>
+    )
+}
+
+export default Footer;
