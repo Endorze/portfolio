7bdaefba5f6eeeb9b2d62dd85556a4ee16aac956
Endorze <alexyurihallgren@hotmail.com>
2025-04-24 16:31:50 +0200

    feat: Finished pokemon section, created new components.

--- frontend/src/components/Portrait/portrait.jsx
+++ frontend/src/components/Portrait/portrait.jsx
@@ -1,14 +1,19 @@
+import { Link } from "react-router-dom";
+import Button from "../Button/button";
 import styles from "./portrait.module.css"
+import H2Title from "../H2Title/h2Title"
 
 const Portrait = ({title, desc, imgPath, interviewText, name, companyLogoPath}) => {
     return (
         <div className={styles.portrait}>
-            <h2>{title}</h2>
+            <H2Title text={title}/>
             <p>{desc}</p>
             <img className={styles.portraitPicture} src={imgPath}/>
-            <p className={styles.interview}><em>"{interviewText}"</em></p>
-            <p><strong>{name}</strong></p>
+            <p className={styles.interview}><em>“{interviewText}“ <br/> - {name}</em></p>
             <img className={styles.companyLogo} src={companyLogoPath} />
+            <Link>
+                <Button text={"Learn More"}/>
+            </Link>
         </div>
     )
 }
