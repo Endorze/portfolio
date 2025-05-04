ae90f5d229c59bb66a3acb62ac84f83b4f09ae39
Endorze <alexyurihallgren@hotmail.com>
2025-05-04 18:21:12 +0200

    feat: Started working on feature section.

---
+++ frontend/src/constants/index.tsx
@@ -0,0 +1,35 @@
+export const features = [
+    {
+        id: "0",
+        icon: "/images/feature-1.png",
+        caption: "Demo Caption",
+        title: "feature 1",
+        text: "text 1",
+        button: {
+            icon: "/images/magictouch.svg",
+            title: "Watch the Demo"
+        }
+    },
+    {
+        id: "1",
+        icon: "/images/feature-1.png",
+        caption: "Demo Caption",
+        title: "feature 1",
+        text: "text 1",
+        button: {
+            icon: "/images/magictouch.svg",
+            title: "Watch the Demo"
+        }
+    },
+        {
+        id: "2",
+        icon: "/images/feature-1.png",
+        caption: "Demo Caption",
+        title: "feature 1",
+        text: "text 1",
+        button: {
+            icon: "/images/magictouch.svg",
+            title: "Watch the Demo"
+        }
+    }
+]
