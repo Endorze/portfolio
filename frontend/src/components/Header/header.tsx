051e60f5ef2a0ebdaf1dce3c382347ee4867abed
Endorze <alexyurihallgren@hotmail.com>
2025-05-05 14:59:54 +0200

    feat: Finished feature section, fixed header

--- frontend/src/components/Header/header.tsx
+++ frontend/src/components/Header/header.tsx
@@ -1,31 +1,42 @@
 import clsx from "clsx";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link as LinkScroll } from "react-scroll";
 
 type Props = {
     title: string
 }
 
-const NavLink = ({ title }: Props) => (
-    <LinkScroll to={""} className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5">
-        {title}
-    </LinkScroll>
-)
-
 const Header = () => {
 
     const [isOpen, setIsOpen] = useState(false);
+    const [hasScroll, setHasScroll] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setHasScroll(window.scrollY > 32)
+        }
+        window.addEventListener('scroll', handleScroll)
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+        }
+    }, [])
+
+    const NavLink = ({ title }: Props) => (
+        <LinkScroll onClick={() => setIsOpen(false)} to={title} offset={-100} spy smooth activeClass="nav-active" className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5">
+            {title}
+        </LinkScroll>
+    )
 
     return (
-        <header className="fixed top-0 left-0 z-50 w-full py-10">
+        <header className={clsx("fixed top-0 left-0 z-50 w-full py-10 transition-all duration-500 max-lg:py-4", hasScroll && "py-2 bg-black-100 backdrop-blur-[8px]")}>
             <div className="container flex h-14 items-center max-lg:px-5">
                 <a className="lg:hidden flex-1 cursor-pointer z-2">
                     <img src="/images/xora.svg" width={115} height={115} alt="logo"></img>
                 </a>
-                <div className={clsx('w-full max-lg:fixed max-lg:top-0 max:lg left-0 max-lg:w-full max-lg:bs-s2 max-lg:opacity-0', isOpen ? "max-lg:opacity-100" : "max-lg:pointer-events-none")}>
-                    <div className="max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4">
+                <div className={clsx('w-full max-lg:fixed max-lg:top-0 max:lg left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0', isOpen ? "max-lg:opacity-100" : "max-lg:pointer-events-none")}>
+                    <div className="w-full max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4">
                         <nav className="max-lg:relative max-lg:z-2 max-lg:my-auto">
-                            <ul className="flex max-lg:block max-lg: px-12">
+                            <ul className="flex max-lg:block max-lg:px-12">
                                 <li className="nav-li">
                                     <NavLink title={"features"} />
                                     <div className="dot" />
@@ -33,7 +44,7 @@ const Header = () => {
                                 </li>
                                 <li className="nav-logo">
                                     <LinkScroll to="hero"
-                                    offset={-100} spy smooth className={clsx("max-lg:hidden transition-transform duration-500 cursor-pointer")}>
+                                    offset={-250} spy smooth className={clsx("max-lg:hidden transition-transform duration-500 cursor-pointer")}>
                                         <img src="/images/xora.svg" width={160} height={55} alt="logo"></img>
                                     </LinkScroll>
                                 </li>
