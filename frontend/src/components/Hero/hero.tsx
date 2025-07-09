4fbd2590e4cfabfd19a3b2724084a84f6b3305b0
Endorze <alexyurihallgren@hotmail.com>
2025-07-09 17:51:41 +0200

    feat: Removed bad padding, made animated text responsive

--- frontend/src/components/Hero/hero.tsx
+++ frontend/src/components/Hero/hero.tsx
@@ -2,10 +2,10 @@ import AnimatedText from "../AnimatedText/animatedText";
 
 const Hero = () => {
     return (
-          <div className="w-full bg-gray-300 py-16">
+          <div className="w-full bg-gray-300">
             <div className="container">
                 <div className="flex justify-center">
-                    <section className="relative pt-40 max-md:pt-4 pb-20 max-md: pb-32 max-w-[1220px]">
+                    <section className="relative pt-40 max-md:pt-4 pb-20 max-md:max-w-[1220px]">
                         <div className="flex flex-col-reverse sm:flex-row justify-between">
 
                             <div className="relative z-2 max-w-512 max-lg:max-w-388 flex flex-col justify-center max-md:pt-6">
@@ -15,7 +15,9 @@ const Hero = () => {
                                 <h1 className="mb-6 h1 text-p4 uppercase max-lg:mb-7 max-lg:h2 max-md:mb-4 max-md:text-5xl max-md:leading-12">
                                     Amazingly Simple
                                 </h1>
+                                <div className="min-h-[50px]">
                                 <AnimatedText text="Welcome to my portfolio page, please make sure to checkout my projects :)" />
+                                </div>
                             </div>
                             <div>
                                 <img src="/gibli.png" className="max-lg:h-auto w-[600px]" alt="hero" />
