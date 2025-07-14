75ddcd32f74a1f5d8ef6fb6eee35b42c8f1978f1
Endorze <alexyurihallgren@hotmail.com>
2025-07-14 15:33:18 +0200

    feat: Created animate on scroll component with AOS

--- frontend/src/pages/Home/home.tsx
+++ frontend/src/pages/Home/home.tsx
@@ -4,14 +4,24 @@ import Footer from "../../components/Footer/footer";
 import ImageCarousel from "../../components/ImageCarousel/imageCarousel";
 import PokemonSection from "../../components/PokemonSection/pokemonSection";
 import Hero from "../../components/Hero/hero";
+import AnimatedScrollComponent from "../../components/AnimatedScrollComponent/animatedScrollComponent";
 
 const Home: FC = () => {
 
     return (
         <div>
             <Hero />
+            <AnimatedScrollComponent>
+
                 <ImageCarousel title={"View My Projects"} text={"I craft my webpages with great passion, i usually take inspiration from games that i've played and that i really enjoyed. To combine my hobbies of programming and gaming."} />
+            </AnimatedScrollComponent>
             <PokemonSection />
+            <AnimatedScrollComponent>
+                <div className="container">
+                    <h1>Test test funkar den?</h1>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius molestiae impedit doloremque nobis sapiente consectetur? Esse, aut. Tempora dolorem, reprehenderit fugiat, debitis possimus at explicabo laboriosam nihil, alias vitae voluptatem?</p>
+                </div>
+            </AnimatedScrollComponent>
         </div>
     )
 }
